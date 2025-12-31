f8eead5565f98acca3cc511233cf3f0c629bb72e
G-Talks <kilic.talha.omer@gmail.com>
2025-12-31 06:25:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,45 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = 'gitalks-app-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
+
+// Yükleme (Install)
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      return cache.add('index.html');
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Dosyalar önbelleğe alındı');
+        return cache.addAll(urlsToCache);
+      })
   );
+  self.skipWaiting(); // Yeni versiyonu hemen aktif et
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+// Aktifleştirme (Activate) - Eski cache'leri temizle
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
+  );
+  self.clients.claim();
+});
+
+// Getirme (Fetch)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
